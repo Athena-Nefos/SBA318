1a124dc79bb1b2e81b220642275397cbd6bf71c7
Athena Nefos <athena.nefos@gmail.com>
2025-01-22 22:43:40 -0500

    middleware added

--- index.js
+++ index.js
@@ -11,12 +11,23 @@ const comments = require("./routes/comments");
 // parsing Middleware
 app.use(bodyParser.json({extended: true }));
 
+//Custom Middleware
+app.use((req, res, next) => {
+    console.log("Request received at: " + Date.now());
+    next();
+});
 
 //routes
 app.use("/api/users", users);
 app.use("/api/posts", posts);
 app.use("/api/comments", comments);
 
+//Error Handling Middleware
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send("Something broke!");
+});
 
 
 
